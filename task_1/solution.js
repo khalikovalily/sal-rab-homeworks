dc8a6e36c8706f3ee803bc587858ab45e5933cd0
KhalikovaLD <khalikova.lily@gmail.com>
2023-07-22 16:37:21 +0400

    task_1_solution_v1

--- task_1/solution.js
+++ task_1/solution.js
@@ -1,6 +1,9 @@
 function addInCartMessage(title, price) {
     let productName = title;
     let productPrice = price;
+    let message = "title за price теперь в корзине!";
+    console.log(message);
+
 
     // Задание №1.1. Оповещение о добавлении в корзину
 
@@ -23,6 +26,7 @@ function addInCartCountChange(value) {
 
     // Задание №1.2. Увеличение количества в корзине
     let oldValue = value;
+    let newValue = (oldValue + 1);
 
     // создайте переменную newValue
     // присвойте ей значение oldValue
@@ -36,6 +40,8 @@ function addInCartCountChange(value) {
 function addInCartSumChange(sum, delta) {
     let oldSum = sum;
     let difference = delta;
+    let newSum = (sum + delta);
+    let newSumText = "(newSum title за price теперь в корзине!"
 
     // Задание №1.3. Увеличение суммы в корзине
 
